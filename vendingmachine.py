a9ed38980fef8ec0ad0b8bf2d9f501439557c78e
hith7 <92586193+hith7@users.noreply.github.com>
2021-12-07 10:09:57 -0600

    Add files via upload

---
+++ vendingmachine.py
@@ -0,0 +1,40 @@
+print("""Welcome to the Ozarks Technical Community College vending machine!
+    |---------------------------------------|
+    |           OTC VENDING MACHINE         |
+    |---------------------------------------|
+    |A1🥤              A2🥤             A3🥤|
+    |---------------------------------------|
+    |B1🥤              B2🥤             B3🥤|                                       
+    |---------------------------------------|
+    |C1🥤              C2🥤             C3🥤|                                       
+    |---------------------------------------|
+    |D1🍿              D2🍿              D3🍿|                                       
+    |---------------------------------------|
+    |E1🍿              E2🍿              E3🍿|                                       
+    |---------------------------------------|
+    |F1🚰              F2🚰              F3🚰|                                       
+    |---------------------------------------|
+    |G1🚰              G2🚰              G3🚰|                                       
+    |---------------------------------------|
+    |  |---------------------------------|  |
+    |  |||||||Open to collect item!|||||||  |  
+    |  |---------------------------------|  |
+    |---------------------------------------|
+    """)
+Selection=input("Please make a selection!")
+if Selection == "A1" or Selection == "A2" or Selection =="A3":
+    print("You chosen Barq's Rootbeer. Please insert $1.50 to receive your product, thank you!")
+if Selection == "B1" or Selection == "B2" or Selection =="B3":
+    print("You chosen Dr Pepper. Please insert $1.50 to receive your product, thank you!")
+if Selection == "C1" or Selection == "C2" or Selection =="C3":
+    print("You chosen Fanta Orange. Please insert $1.50 to receive your product, thank you!")
+if Selection == "D1" or Selection == "D2" or Selection =="D3":
+    print("You chosen Super Butter Popcorn!. Please insert $1.50 to receive your product, thank you!")
+if Selection == "E1" or Selection == "E2" or Selection =="E3":
+    print("You chosen Kettle Corn!. Please insert $1.50 to receive your product, thank you!")
+if Selection == "F1" or Selection == "F2" or Selection =="F3":
+    print("You chosen a Basic Water Bottle.. Please insert $1.00 to receive your product, thank you!")
+if Selection == "G1" or Selection == "G2" or Selection =="G3":
+    print("You chosen a Nestle Water Bottle.. Please insert $1.00 to receive your product, thank you!")
+
+
